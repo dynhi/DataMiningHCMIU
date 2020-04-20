12e10cd71fb5868e0c6018f977c994fad24cb978
Đoàn Ý Nhi <48195940+dynhi@users.noreply.github.com>
2020-04-21 04:22:22 +0700

    Add files via upload

--- src/Classification.java
+++ src/Classification.java
@@ -1,85 +1,90 @@
-import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.rules.ZeroR;
 import weka.classifiers.trees.J48;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 import java.util.Random;
 
 public class Classification {
-    public static String path = "src/data/Autism-Adult-Data-binary.arff";
-    private static DataSource getSource(String path) throws Exception{
-        return new DataSource(path);
-    }
-    private static Instances getData(String path) throws Exception {
-        DataSource source = getSource(path);
-        return source.getDataSet();
-    }
+    public static String path = "src/data/final.arff";
+    private static DataSource source;
+    private static Instances data;
+    private static Evaluation eval;
+
     public static void main(String[] args) throws Exception{
-        Instances data = getData(path);
-        data.setClassIndex(data.numAttributes() - 1);
+        zeroRClassifier();
+        naiveBayesClassifier();
+        j48Classifier();
 
-        Evaluation eval = new Evaluation(data);
-
-        NaiveBayes nbModel = NaiveBayesClassifier();
-        eval.crossValidateModel(nbModel, data, 10, new Random(1));
-
-        System.out.println("Naive Bayes Model");
-        System.out.println(nbModel);
-
-        eval.toMatrixString("=== Confusion Matrix ===\n");
-
-        J48 j48model = J48Classifier();
-        j48model.buildClassifier(data);
-        eval.crossValidateModel(j48model, data, 10, new Random(1));
-
-        System.out.println("J48 Model");
-        System.out.println(j48model);
-
-        eval.toMatrixString("=== Confusion Matrix ===\n");
-
-        ZeroR baseline = ZeroRClassifier();
-        baseline.buildClassifier(data);
-        eval.crossValidateModel(baseline, data, 10, new Random(1));
-
-        System.out.println("ZeroR Model");
-        System.out.println(baseline);
-        eval.toMatrixString("=== Confusion Matrix ===\n");
     }
 
-    public static NaiveBayes NaiveBayesClassifier() throws Exception{
+    public static void naiveBayesClassifier() throws Exception{
+        source = new DataSource(path);
+        data = source.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
         NaiveBayes model = new NaiveBayes();
         String option[] = new String[]{"-K"};
         model.setOptions(option);
 
-        return model;
+        eval = new Evaluation(data);
+        eval.crossValidateModel(model, data, 10, new Random(1));
+        model.buildClassifier(data);
+        System.out.println("=== Naive Bayes Model ===\n");
+        System.out.println(model);
+        printConfusionMatrix();
     }
 
-    public static J48 J48Classifier() throws Exception{
-        J48 model = new J48();
-        String option[] = new String[]{"-U", "-C", "0.25", "M", "2"};
-        model.setOptions(option);
+    public static void j48Classifier() throws Exception{
+        source = new DataSource(path);
+        data = source.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
+        J48 tree = new J48();
+        String option[] = new String[4];
+        option[0] = "-C";
+        option[1] = "0.25";
+        option[2] = "-M";
+        option[3] = "2";
+        tree.setOptions(option);
 
-        return model;
+        eval = new Evaluation(data);
+        eval.crossValidateModel(tree, data, 10, new Random(1));
+        tree.buildClassifier(data);
+        System.out.println("=== J48 Model ===\n");
+        System.out.println(tree);
+        System.out.println(tree.graph());
+        printConfusionMatrix();
     }
 
-    private static ZeroR ZeroRClassifier() throws Exception{
-        return new ZeroR();
+    private static void zeroRClassifier() throws Exception{
+        source = new DataSource(path);
+        data = source.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
+        ZeroR baseline = new ZeroR();
+
+        eval = new Evaluation(data);
+        eval.crossValidateModel(baseline, data, 10, new Random(1));
+        baseline.buildClassifier(data);
+        System.out.println("=== ZeroR Model ===\n");
+        System.out.println(baseline);
+        printConfusionMatrix();
     }
 
-//    public static void printNaiveBayes(){
-//        // Get NaiveBayes
-//    NaiveBayes nb = NaiveBayesClassifier();
-//
-//    // Train Naive Bayes
-//    nb.buildClassifier(data);
-//
-//    // Output generated model
-//    System.out.println(nb);
-//    }
-
-
+    private static void printConfusionMatrix() throws Exception{
+        System.out.println();
+        System.out.println(eval.toMatrixString("=== Confusion matrix for fold ===\n"));
+        System.out.println("Correct % = "+eval.pctCorrect());
+        System.out.println("Incorrect % = "+eval.pctIncorrect());
+        System.out.println("AUC = "+eval.areaUnderROC(1));
+        System.out.println("kappa = "+eval.kappa());
+        System.out.println("MAE = "+eval.meanAbsoluteError());
+        System.out.println("RMSE = "+eval.rootMeanSquaredError());
+        System.out.println("RAE = "+eval.relativeAbsoluteError());
+        System.out.println("RRSE = "+eval.rootRelativeSquaredError());
+        System.out.println("Precision = "+eval.precision(1));
+        System.out.println("Recall = "+eval.recall(1));
+        System.out.println("fMeasure = "+eval.fMeasure(1));
+        System.out.println("Error Rate = "+eval.errorRate());
+    }
 }
