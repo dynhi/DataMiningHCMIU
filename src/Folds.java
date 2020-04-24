0f18a7daf3d4d7879847bdb0e9c52e4d0bf025db
Đoàn Ý Nhi <48195940+dynhi@users.noreply.github.com>
2020-04-24 19:02:14 +0700

    Update Folds.java

--- src/Folds.java
+++ src/Folds.java
@@ -1,5 +1,7 @@
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.rules.ZeroR;
+import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -8,13 +10,16 @@ import java.util.Random;
 public class Folds {
     public static void main(String args[]) throws Exception{
         //load dataset
-        DataSource source = new DataSource("src/data/Autism-Adult-Data-question1.arff");
+        DataSource source = new DataSource("src/data/final.arff");
         Instances dataset = source.getDataSet();
         //set class index to the last attribute
         dataset.setClassIndex(dataset.numAttributes()-1);
 
         //create the classifier
-        NaiveBayes nb = new NaiveBayes();
+        //choose the executed algorithm
+        //NaiveBayes model = new NaiveBayes();
+        //J48 model = new J48();
+        ZeroR model = new ZeroR();
 
         int seed = 1;
         int folds = 10;
@@ -34,24 +39,24 @@ public class Folds {
             Instances train = randData.trainCV(folds, n);
             Instances test = randData.testCV(folds, n);
             // build and evaluate classifier
-            nb.buildClassifier(train);
-            eval.evaluateModel(nb, test);
+            model.buildClassifier(train);
+            eval.evaluateModel(model, test);
 
             // output evaluation
             System.out.println();
             System.out.println(eval.toMatrixString("=== Confusion matrix for fold " + (n+1) + "/" + folds + " ===\n"));
             System.out.println("Correct % = "+eval.pctCorrect());
-            System.out.println("Incorrect % = "+eval.pctIncorrect());
-            System.out.println("AUC = "+eval.areaUnderROC(1));
-            System.out.println("kappa = "+eval.kappa());
-            System.out.println("MAE = "+eval.meanAbsoluteError());
-            System.out.println("RMSE = "+eval.rootMeanSquaredError());
-            System.out.println("RAE = "+eval.relativeAbsoluteError());
-            System.out.println("RRSE = "+eval.rootRelativeSquaredError());
+//            System.out.println("Incorrect % = "+eval.pctIncorrect());
+//            System.out.println("AUC = "+eval.areaUnderROC(1));
+//            System.out.println("kappa = "+eval.kappa());
+//            System.out.println("MAE = "+eval.meanAbsoluteError());
+//            System.out.println("RMSE = "+eval.rootMeanSquaredError());
+//            System.out.println("RAE = "+eval.relativeAbsoluteError());
+//            System.out.println("RRSE = "+eval.rootRelativeSquaredError());
             System.out.println("Precision = "+eval.precision(1));
             System.out.println("Recall = "+eval.recall(1));
             System.out.println("fMeasure = "+eval.fMeasure(1));
-            System.out.println("Error Rate = "+eval.errorRate());
+//            System.out.println("Error Rate = "+eval.errorRate());
             //the confusion matrix
             //System.out.println(eval.toMatrixString("=== Overall Confusion Matrix ===\n"));
         }
